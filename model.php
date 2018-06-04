758d26fd8c1229141d7aab6cf3fbc62e4feea874
Monica <monicaivan25@gmail.com>
2018-06-04 20:36:07 +0300

    updatat functii anunt
    adaugat parametru sex
    creat functie pt postat anunt fara poza

--- model.php
+++ model.php
@@ -37,11 +37,40 @@ function getBookmarkById($id){
     return $rows;
 }
 
-function insertData($numar_telefon, $oras, $judet, $nume_animal, $tip_animal, $rasa, $pret, $tip_anunt, $descriere, $target_file){
+function insertData($numar_telefon, $oras, $judet, $nume_animal, $tip_animal, $sex_animal, $rasa, $pret, $tip_anunt, $descriere, $target_file){
     global $conn;
 
-    $result=mysqli_query($conn,"INSERT INTO `anunturi` ( `numar_telefon`, `oras`, `judet`, `nume_animal`, `tip_animal`, `rasa`, `pret`, `tip_anunt`, `descriere`,`path`) VALUES ('".$numar_telefon."','".$oras."','" .$judet."','".$nume_animal."', '".$tip_animal."', '".$rasa."', '".$pret."', '".$tip_anunt."', '".$descriere."','".$target_file."')");
+    $result=mysqli_query($conn,"INSERT INTO `anunturi` ( `numar_telefon`, `oras`, `judet`, `nume_animal`, `tip_animal`, `sex_animal`, `rasa`, `pret`, `tip_anunt`, `descriere`,`path`) VALUES ('".$numar_telefon."','".$oras."','" .$judet."','".$nume_animal."', '".$tip_animal."', '".$sex_animal."', '".$rasa."', '".$pret."', '".$tip_anunt."', '".$descriere."','".$target_file."')");
+   
+    return $result;
+}
+function insertDataNoPic($numar_telefon, $oras, $judet, $nume_animal, $tip_animal, $sex_animal, $rasa, $pret, $tip_anunt, $descriere){
+    global $conn;
+
+    $result=mysqli_query($conn,"INSERT INTO `anunturi` ( `numar_telefon`, `oras`, `judet`, `nume_animal`, `tip_animal`, `sex_animal`, `rasa`, `pret`, `tip_anunt`, `descriere`) VALUES ('".$numar_telefon."','".$oras."','" .$judet."','".$nume_animal."', '".$tip_animal."', '".$sex_animal."', '".$rasa."', '".$pret."', '".$tip_anunt."', '".$descriere."')");
+   
     return $result;
 }
 
+
+/*function sendFilters($sex, $pret, $judet){
+    global $conn;
+
+    $result=mysqli_query($conn,"select rname, parola from users;");
+    //echo $result;
+    //echo ;
+    
+    if(mysqli_num_rows($result) > 0){
+        //output for each row
+        while($row = mysqli_fetch_assoc($result)) {
+            echo $row["username"] . "    " . $row["parola"] . " <br>";
+        } 
+    } else
+    {
+        echo "0 results";
+    }
+
+}
+
 ?>
+*/
