a8494b8f56872231ac248e8de0a3e6c61e4bfd79
Monica <monicaivan25@gmail.com>
2018-06-04 20:29:51 +0300

    added gender

--- anunt.php
+++ anunt.php
@@ -16,11 +16,11 @@
 	$judet = $_POST['judet'];
 	$nume_animal = $_POST['nume_animal'];
 	$tip_animal = $_POST['tip_animal'];
+  $sex_animal = $_POST['sex_animal'];
 	$rasa = $_POST['rasa'];
 	$pret = $_POST['pret'];
 	$tip_anunt = $_POST['tip_anunt'];
 	$descriere = $_POST['descriere'];
-
 	//if connected then Select Database.
 	//$db=mysql_select_db("tw_2018",$con);
 	$target_dir = "uploads/";
@@ -47,15 +47,15 @@ if ($_FILES["fileToUpload"]["size"] > 500000000) {
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
     $_SESSION['Error'] = "Poza nu s-a putut incarca, doar fisierele de tip JPG, JPEG, PNG & GIF sunt permise.";
-    $uploadOk = 0;
+   // $uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk != 0) {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-		$result = insertData($numar_telefon, $oras, $judet, $nume_animal, $tip_animal, $rasa, $pret, $tip_anunt, $descriere, $target_file);    
-	} else {
-        $_SESSION['Error'] = "Auleu buba la up.";
-    }
+		  $result = insertData($numar_telefon, $oras, $judet, $nume_animal, $tip_animal, $sex_animal, $rasa, $pret, $tip_anunt, $descriere, $target_file);
+    
+	} else 
+  $result = insertDataNoPic($numar_telefon, $oras, $judet, $nume_animal, $tip_animal, $sex_animal, $rasa, $pret, $tip_anunt, $descriere);
 }
 
 	mysqli_close($conn);
