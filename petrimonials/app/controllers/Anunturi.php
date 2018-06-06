d05e1aeaf9504d9c3871a631e73ef3c7257def43
Monica <monicaivan25@gmail.com>
2018-06-06 20:23:34 +0300

    facut sa mearga paginile de anunt dinamic

--- petrimonials/app/controllers/Anunturi.php
+++ petrimonials/app/controllers/Anunturi.php
@@ -5,8 +5,22 @@
     {
         $modelcon = $this->model('Model');
       
-        $data = $modelcon->iaAnunt($id);
+        $anuntCurent = $modelcon->iaAnunt($id);
+        $anunturiSimilare = $modelcon->iaAnuntSimilar($anuntCurent);
+        shuffle($anunturiSimilare);
 
+        $i=0;
+        $data = [];
+        array_push($data, $anuntCurent);
+        foreach ($anunturiSimilare as $anunt) {
+          array_push($data, $anunt);
+          $i += 1;
+        }
+
+        while($i < 4){
+          $i += 1;
+          array_push($data, $anuntCurent);
+        }
         $this->view('home/anunturi', $data);
         $modelcon->closeCon();
     }
