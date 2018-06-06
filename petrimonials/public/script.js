b6fe74d0a18f6a17d37832dea1e273498e7a60ba
ApuscasiteiSilviu <silviu.alex95@yahoo.ro>
2018-06-06 11:35:13 +0300

    adaugat functia de inregistrare

--- petrimonials/public/script.js
+++ petrimonials/public/script.js
@@ -19,3 +19,81 @@ function collectNewForm(){
   
 };
 
+function Inregistrare(){
+
+	var par1 = document.getElementById("par1-id").value;
+    var par2 = document.getElementById("par2-id").value;
+    var username = document.getElementById("user-id").value;
+    var email = document.getElementById("email-id").value;
+   
+   let xhr = new XMLHttpRequest();
+
+		xhr.open("POST", "http://localhost:81/petrimonials/public/GetEmail/verificaEmail");
+
+		xhr.addEventListener("load", function loadCallback() {
+		    switch (xhr.status) {
+		        case 200:
+		            console.log("Succ " + xhr.response);
+		            if (xhr.response == 0){
+						alert("Esti inregistrat deja");
+						document.getElementById('email-id').value = '';
+						console.log("esti pe if-ul pentru verificaEmail");
+					}else{
+						
+						//parolele nu corespund
+					    if (par1 !== par2){
+					    	alert("Parolele nu corespund");
+					    	document.getElementById('par1-id').value = '';
+					    	document.getElementById('par2-id').value = '';
+					    	console.log("parolele nu corespund");
+
+					    } else {
+					    	let xhr2 = new XMLHttpRequest();
+
+							xhr2.open("POST", "http://localhost:81/petrimonials/public/Inregistrare/singUp");
+
+							xhr2.addEventListener("load", function loadCallback() {
+							    switch (xhr2.status) {
+							        case 200:
+							            console.log("Success, ai inserat in baza de date" + xhr2.response);
+							            break;
+							        case 404:
+							            console.log("Oups! Not found");
+							            break;
+							    }
+							});
+
+							xhr2.addEventListener("error", function errorCallback() {
+							    console.log("Network error");
+							});
+
+							let payload = {
+					            username: `${username}`,
+					            email: `${email}`,
+					            pass: `${par1}`
+					        }
+							xhr2.send(JSON.stringify(payload));
+							document.getElementById('signup').style.display='none';
+					    	}
+				    }
+
+		            break;
+		        case 404:
+		            console.log("Oups! Not found");
+		            break;
+		    }
+		});
+
+		xhr.addEventListener("error", function errorCallback() {
+		    console.log("Network error");
+		});
+
+		let payload = {
+            email: `${email}`
+        }
+		xhr.send(JSON.stringify(payload));
+
+	
+      
+}
+
